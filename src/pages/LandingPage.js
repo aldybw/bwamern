82cd6c67e91a5bac2916e36a39854ccd00b19c55
Aldycky1 <bagusaldycky@gmail.com>
2021-03-10 08:23:23 +0700

    Created Header Component and put to arrange LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 
+import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }
