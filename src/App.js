f7fe3d36453da4589d3e55bbe09ca919651a9f26
Aldycky1 <bagusaldycky@gmail.com>
2021-03-16 08:34:38 +0700

    Finished Checkout Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 import Example from "pages/Example";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/properties/:id" component={DetailsPage} />
-        {/* <Route exact path="/example" component={Example} /> */}
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/example" component={Example} />
       </Router>
     </div>
   );
--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -5,6 +5,7 @@ import propTypes from "prop-types";
 export default function Button(props) {
   const className = [props.className];
   if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
   if (props.isLarge) className.push("btn-lg");
   if (props.isSmall) className.push("btn-sm");
   if (props.isBlock) className.push("btn-block");
@@ -80,6 +81,7 @@ Button.propTypes = {
   isSmall: propTypes.bool,
   isLarge: propTypes.bool,
   isBlock: propTypes.bool,
+  isLight: propTypes.bool,
   isExternal: propTypes.bool,
   hasShadow: propTypes.bool,
 };
