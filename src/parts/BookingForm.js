78ea01bfba7b1154a0e7277eed1dae0be634b708
Aldycky1 <bagusaldycky@gmail.com>
2021-09-17 20:21:05 +0700

    updated bookingform part

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 import propTypes from "prop-types";
 
 import Button from "elements/Button";
 import { InputNumber, InputDate } from "elements/Form";
 
-export default class BookingForm extends Component {
+class BookingForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -134,3 +135,5 @@ BookingForm.propTypes = {
   itemDetails: propTypes.object,
   startBooking: propTypes.func,
 };
+
+export default withRouter(BookingForm);
