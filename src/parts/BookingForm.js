6e19973aaad0439fd9e4ae9ea1716c9adecd9aca
Aldycky1 <bagusaldycky@gmail.com>
2021-09-17 13:33:47 +0700

    booking startDate endDate with redux

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -63,6 +63,18 @@ export default class BookingForm extends Component {
     }
   }
 
+  startBooking = () => {
+    const { data } = this.state;
+    this.props.startBooking({
+      _id: this.props.itemDetails._id,
+      duration: data.duration,
+      date: {
+        startDate: data.date.startDate,
+        endDate: data.date.endDate,
+      },
+    });
+  };
+
   render() {
     const { data } = this.state;
     const { itemDetails, startBooking } = this.props;
@@ -109,7 +121,7 @@ export default class BookingForm extends Component {
           hasShadow
           isPrimary
           isBlock
-          onClick={startBooking}
+          onClick={this.startBooking}
         >
           Continue to book
         </Button>
