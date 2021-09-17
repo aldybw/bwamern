b04b350ad4a83c94ac09372f336aa4d1806322f0
Aldycky1 <bagusaldycky@gmail.com>
2021-09-17 09:15:31 +0700

    update staycation front-end

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
+import Example from "pages/Example";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/example" component={Example} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
