8db4c711adbe8d0e5066c1b17447bf43e26784e1
Aldycky1 <bagusaldycky@gmail.com>
2021-09-18 13:22:14 +0700

    making details-page with api

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -11,16 +11,25 @@ import Categories from "parts/Categories";
 import Testimony from "parts/Testimony";
 import Footer from "parts/Footer";
 
-import ItemDetails from "json/itemDetails.json";
-
 import { checkoutBooking } from "store/actions/checkout";
+import { fetchPage } from "store/actions/page";
 
 class DetailsPage extends Component {
   componentDidMount() {
     window.title = "Details Page";
     window.scrollTo(0, 0);
+
+    if (!this.props.page[this.props.match.params.id])
+      this.props.fetchPage(
+        `${process.env.REACT_APP_HOST}/api/v1/member/detail-page/${this.props.match.params.id}`,
+        this.props.match.params.id
+      );
   }
   render() {
+    const { page, match } = this.props;
+    console.log(page);
+    if (!page[match.params.id]) return null;
+
     const breadcrumb = [
       { pageTitle: "Home", pageHref: "" },
       { pageTitle: "House Details", pageHref: "" },
@@ -29,31 +38,37 @@ class DetailsPage extends Component {
     return (
       <>
         <Header {...this.props} />
-        <PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
-        <FeaturedImage data={ItemDetails.imageUrls} />
+        <PageDetailTitle breadcrumb={breadcrumb} data={page[match.params.id]} />
+        <FeaturedImage data={page[match.params.id].imageId} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr-5">
               <Fade bottom>
-                <PageDetailDescription data={ItemDetails} />
+                <PageDetailDescription data={page[match.params.id]} />
               </Fade>
             </div>
             <div className="col-5">
               <Fade bottom>
                 <BookingForm
-                  itemDetails={ItemDetails}
+                  itemDetails={page[match.params.id]}
                   startBooking={this.props.checkoutBooking}
                 />
               </Fade>
             </div>
           </div>
         </section>
-        <Categories data={ItemDetails.categories} />
-        <Testimony data={ItemDetails.testimonial} />
+        <Categories data={page[match.params.id].activityId} />
+        <Testimony data={page[match.params.id].testimonial} />
         <Footer />
       </>
     );
   }
 }
 
-export default connect(null, { checkoutBooking })(DetailsPage);
+const mapStateToProps = (state) => ({
+  page: state.page,
+});
+
+export default connect(mapStateToProps, { checkoutBooking, fetchPage })(
+  DetailsPage
+);
